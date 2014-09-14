703e3db4e5c6837d81d668eb0716d72e9b824192
nick <nick@argos.io>
2014-09-14 12:24:12 +1000

    removed node_modules and updated dependencies

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 (function(module) {
-	"use strict";
+	'use strict';
 
 	var User = module.parent.require('./user'),
 		Topics = module.parent.require('./topics'),
@@ -12,6 +12,7 @@
 		winston = module.parent.require('winston'),
 		async = module.parent.require('async'),
 		SlackClient = require('node-slack'),
+		slack = null,
 
 		constants = Object.freeze({
 			name : 'slack',
@@ -22,14 +23,12 @@
 			}
 		});
 	
-	var slack = null,
-		Slack = {};
+	var Slack = {};
 
 	Slack.init = function(app, middleware, controllers, callback) {
 		function render(req, res, next) {
 			res.render('admin/plugins/slack', {});
 		}
-
 		app.get('/admin/plugins/slack', middleware.admin.buildHeader, render);
 		app.get('/api/admin/plugins/slack', render);
 		slack = new SlackClient(meta.config['slack:domain'], meta.config['slack:token'])
@@ -41,60 +40,54 @@
 
 		async.parallel({
 			user: function(callback) {
-				// retrieve username, email and profile image url
+				// retrieve user
 				User.getUserData(post.uid, function(err, user) {
 					if (err) { return callback(err); }
 					var data = {
 						username : user.username,
 						email    : user.email,
-						image    : user.picture || ""
+						image    : user.picture.match(/^\/\//) ? 'http:' + user.picture : user.picture;
 					};
-					// add the protocol portion of the url if its missing then prepend it
-					if (data.image.match(/^\/\//)) { data.image = "http:" + data.image; }
-
 					return callback(null, data);
 				})
 			},
 			topic: function(callback) {
+				// retrieve topic and category
 				Topics.getTopicData(post.tid, function(err, topic) {
 					if (err) { return callback(err); }
-					// Grab category name
 					Categories.getCategoryData(topic.cid, function(err, category) {
 						if (err) { return callback(err); }
-
 						var data = {
 							title     : topic.title,
 							category  : category.name,
-							link      : nconf.get('url') + "/topic/" + topic.slug,
+							link      : nconf.get('url') + '/topic/' + topic.slug,
 							timestamp : topic.timestamp
 						};
-						return callback(null, data)
+						return callback(null, data);
 					})
 				})	
 			}
-		}, function(err, results) {
-			// construct message: <username> posted [<categoryname> > <topicname>]: <message>
+		}, function(err, data) {
+			// trim message based on config option
 			var maxContentLength = meta.config['slack:post:maxlength'] || false
-			if (maxContentLength && content.length > maxContentLength) {
-				content = content.substring(0, maxContentLength) + "..."
-			}
-			var message = "<" + results.topic.link + "|[" + results.topic.category + ": " + results.topic.title + "]>\n" + content
+			if (maxContentLength && content.length > maxContentLength) { content = content.substring(0, maxContentLength) + '...'; }
+			// message format: <username> posted [<categoryname> : <topicname>]\n <message>
+			var message = '<' + data.topic.link + '|[' + data.topic.category + ': ' + data.topic.title + ']>\n' + content;
 
 			slack.send({
-				"text"     : message,
-				"channel"  : (meta.config['slack:channel'] || "#general"),
-				"username" : results.user.username,
-				"icon_url" : results.user.image
+				'text'     : message,
+				'channel'  : (meta.config['slack:channel'] || '#general'),
+				'username' : data.user.username,
+				'icon_url' : data.user.image
 			});
 		});
-
 	},
 
 	Slack.adminMenu = function(headers, callback) {
 		headers.plugins.push({
-			"route": constants.admin.route,
-			"icon": constants.admin.icon,
-			"name": constants.admin.label
+			'route' : constants.admin.route,
+			'icon'  : constants.admin.icon,
+			'name'  : constants.admin.label
 		});
 		callback(null, headers);
 	}
